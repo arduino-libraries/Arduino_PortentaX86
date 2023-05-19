93ddeb68c7f4b8e9d76d7da5916bdf9fc92d1584
Martino Facchin <m.facchin@arduino.cc>
2023-05-19 16:59:57 +0200

    Rename example

--- examples/Portenta_x86_VM_dualcore/Portenta_x86_VM_dualcore.ino
+++ examples/Portenta_x86_VM_dualcore/Portenta_x86_VM_dualcore.ino
@@ -1,14 +1,15 @@
 #include "Arduino_PortentaX86.h"
-#include "mbed.h"
 
 UART mySerial(PG_14, PG_9);
 
 REDIRECT_STDOUT_TO(mySerial);
 
 static CKernel Kernel;
+
 void setup() {
   mySerial.begin(115200);
-  Kernel.Initialize(false);
+  checkM4Binary();
+  Kernel.Initialize(); 
   Kernel.Run();
 }
 
